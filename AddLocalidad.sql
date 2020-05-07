102d4fb80e6efad76cc271bf30a09a3f81090a21
sedradivaio <56560665+sedradivaio@users.noreply.github.com>
2020-05-07 18:50:45 +0200

    Update AddLocalidad.sql

--- AddLocalidad.sql
+++ AddLocalidad.sql
@@ -1,9 +1,9 @@
 alter table usuario add column localidad varchar (30);
-update usuario set localidad='Ourense' where id_usuario=1;
+/*update usuario set localidad='Ourense' where id_usuario=1;
 update usuario set localidad='Ferrol' where id_usuario=2;
 update usuario set localidad='Cangas' where id_usuario=3;
 update usuario set localidad='Ourense' where id_usuario=4;
 update usuario set localidad='Xinzo' where id_usuario=5;
 update usuario set localidad='Betanzos' where id_usuario=6;
 update usuario set localidad='Ferrol' where id_usuario=7;
-update usuario set localidad='Ferrol' where id_usuario=8;
+update usuario set localidad='Ferrol' where id_usuario=8;*/
