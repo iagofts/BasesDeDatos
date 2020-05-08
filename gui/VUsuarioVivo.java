953f5c05a1d8cbbf11b33f8725c9214f1f45dd06
iagofts <64792716+iagofts@users.noreply.github.com>
2020-05-08 20:39:54 +0200

    Add files via upload

--- gui/VUsuarioVivo.java
+++ gui/VUsuarioVivo.java
@@ -30,11 +30,22 @@ public class VUsuarioVivo extends javax.swing.JFrame {
         jLabel7 = new javax.swing.JLabel();
         jLabel8 = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
-        jTable1 = new javax.swing.JTable();
+        tablaPecados = new javax.swing.JTable();
         jScrollPane2 = new javax.swing.JScrollPane();
-        jTable2 = new javax.swing.JTable();
+        tablaBA = new javax.swing.JTable();
         btnConfesar = new javax.swing.JButton();
         btnSalir = new javax.swing.JButton();
+        jPanelVenganzas = new javax.swing.JPanel();
+        jLabel6 = new javax.swing.JLabel();
+        textoNombreBusqueda = new javax.swing.JTextField();
+        jLabel9 = new javax.swing.JLabel();
+        jScrollPane3 = new javax.swing.JScrollPane();
+        tablaUsuarios = new javax.swing.JTable();
+        btnBuscar = new javax.swing.JButton();
+        jComboBox1 = new javax.swing.JComboBox();
+        jLabel10 = new javax.swing.JLabel();
+        btnSolicitar = new javax.swing.JButton();
+        btnSalirV = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -64,31 +75,11 @@ public class VUsuarioVivo extends javax.swing.JFrame {
 
         jLabel8.setText("Buenas Acciones");
 
-        jTable1.setModel(new javax.swing.table.DefaultTableModel(
-            new Object [][] {
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null}
-            },
-            new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
-            }
-        ));
-        jScrollPane1.setViewportView(jTable1);
+        tablaPecados.setModel(new ModeloTablaPecados());
+        jScrollPane1.setViewportView(tablaPecados);
 
-        jTable2.setModel(new javax.swing.table.DefaultTableModel(
-            new Object [][] {
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null}
-            },
-            new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
-            }
-        ));
-        jScrollPane2.setViewportView(jTable2);
+        tablaBA.setModel(new ModeloTablaBuenasAcciones());
+        jScrollPane2.setViewportView(tablaBA);
 
         btnConfesar.setText("Confesar");
 
@@ -142,7 +133,7 @@ public class VUsuarioVivo extends javax.swing.JFrame {
                                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                                         .addComponent(btnSalir)))
                                 .addComponent(jLabel7)))
-                        .addGap(0, 32, Short.MAX_VALUE))))
+                        .addGap(0, 41, Short.MAX_VALUE))))
         );
         jPanelInformacionLayout.setVerticalGroup(
             jPanelInformacionLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -175,10 +166,99 @@ public class VUsuarioVivo extends javax.swing.JFrame {
                 .addGroup(jPanelInformacionLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(btnConfesar)
                     .addComponent(btnSalir))
-                .addContainerGap(13, Short.MAX_VALUE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         jTabbedPane1.addTab("Informacion", jPanelInformacion);
+
+        jLabel6.setText("Nombre");
+
+        textoNombreBusqueda.setText("jTextField1");
+
+        jLabel9.setText("Usuarios:");
+
+        tablaUsuarios.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jScrollPane3.setViewportView(tablaUsuarios);
+
+        btnBuscar.setText("Buscar");
+
+        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+
+        jLabel10.setText("Venganza");
+
+        btnSolicitar.setText("Solicitar");
+
+        btnSalirV.setText("Salir");
+
+        javax.swing.GroupLayout jPanelVenganzasLayout = new javax.swing.GroupLayout(jPanelVenganzas);
+        jPanelVenganzas.setLayout(jPanelVenganzasLayout);
+        jPanelVenganzasLayout.setHorizontalGroup(
+            jPanelVenganzasLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanelVenganzasLayout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanelVenganzasLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel9)
+                    .addGroup(jPanelVenganzasLayout.createSequentialGroup()
+                        .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 379, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGroup(jPanelVenganzasLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(jPanelVenganzasLayout.createSequentialGroup()
+                                .addGap(149, 149, 149)
+                                .addComponent(btnBuscar))
+                            .addGroup(jPanelVenganzasLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                                .addGroup(jPanelVenganzasLayout.createSequentialGroup()
+                                    .addGap(50, 50, 50)
+                                    .addComponent(btnSolicitar)
+                                    .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 145, Short.MAX_VALUE)
+                                    .addComponent(btnSalirV))
+                                .addGroup(jPanelVenganzasLayout.createSequentialGroup()
+                                    .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                    .addGroup(jPanelVenganzasLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                        .addComponent(jLabel6)
+                                        .addComponent(jLabel10))
+                                    .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                    .addGroup(jPanelVenganzasLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                        .addComponent(textoNombreBusqueda)
+                                        .addComponent(jComboBox1, 0, 252, Short.MAX_VALUE)))))))
+                .addContainerGap(60, Short.MAX_VALUE))
+        );
+        jPanelVenganzasLayout.setVerticalGroup(
+            jPanelVenganzasLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanelVenganzasLayout.createSequentialGroup()
+                .addGap(22, 22, 22)
+                .addComponent(jLabel9)
+                .addGroup(jPanelVenganzasLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addGroup(jPanelVenganzasLayout.createSequentialGroup()
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 285, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanelVenganzasLayout.createSequentialGroup()
+                        .addGap(14, 14, 14)
+                        .addGroup(jPanelVenganzasLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel6)
+                            .addComponent(textoNombreBusqueda, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(btnBuscar)
+                        .addGap(75, 75, 75)
+                        .addGroup(jPanelVenganzasLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel10)
+                            .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGroup(jPanelVenganzasLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(btnSolicitar)
+                            .addComponent(btnSalirV))))
+                .addContainerGap(40, Short.MAX_VALUE))
+        );
+
+        jTabbedPane1.addTab("Venganzas", jPanelVenganzas);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -188,33 +268,44 @@ public class VUsuarioVivo extends javax.swing.JFrame {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jTabbedPane1)
+            .addComponent(jTabbedPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 395, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnBuscar;
     private javax.swing.JButton btnConfesar;
     private javax.swing.JButton btnGuardar;
     private javax.swing.JButton btnSalir;
+    private javax.swing.JButton btnSalirV;
+    private javax.swing.JButton btnSolicitar;
+    private javax.swing.JComboBox jComboBox1;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel10;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
     private javax.swing.JLabel jLabel7;
     private javax.swing.JLabel jLabel8;
+    private javax.swing.JLabel jLabel9;
     private javax.swing.JPanel jPanelInformacion;
+    private javax.swing.JPanel jPanelVenganzas;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JScrollPane jScrollPane3;
     private javax.swing.JTabbedPane jTabbedPane1;
-    private javax.swing.JTable jTable1;
-    private javax.swing.JTable jTable2;
+    private javax.swing.JTable tablaBA;
+    private javax.swing.JTable tablaPecados;
+    private javax.swing.JTable tablaUsuarios;
     private javax.swing.JTextField textoContrasena;
     private javax.swing.JTextField textoFechaNacimiento;
     private javax.swing.JTextField textoLocalidad;
     private javax.swing.JTextField textoNombre;
+    private javax.swing.JTextField textoNombreBusqueda;
     private javax.swing.JTextField textoUsuario;
     // End of variables declaration//GEN-END:variables
 }
