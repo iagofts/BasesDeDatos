63f479df5c6038f0d8e8eb38db25f3440bf04171
sedradivaio <56560665+sedradivaio@users.noreply.github.com>
2020-05-09 16:47:37 +0200

    Add files via upload

---
+++ FachadaGui.java
@@ -0,0 +1,56 @@
+package gui;
+
+import aplicacion.TipoUsuario;
+import aplicacion.Usuario;
+import aplicacion.Vivo;
+
+public class FachadaGui {
+    aplicacion.FachadaAplicacion fa;
+    
+   public FachadaGui(aplicacion.FachadaAplicacion fa){
+     this.fa=fa;
+   } 
+
+    public void iniciaVista(){
+       VAutentificacion va;
+      va = new VAutentificacion(fa);
+      va.setVisible(true);
+    }
+    
+    public void muestraExcepcion(String txtExcepcion){
+       VAviso va;
+       va = new VAviso(txtExcepcion);
+       va.setVisible(true);
+    }
+     public void muestraExcepcion(java.awt.Frame parent, boolean modal, String txtExcepcion){
+        VAviso va;
+       va = new VAviso(parent,modal,txtExcepcion);
+       va.setVisible(true);
+    }
+    public void muestraVPrincipal(Usuario user){
+        if(user.getTipoUsuario().equals(TipoUsuario.Dios)){ //Ventana principal Dios
+            VDios vD;
+            vD=new VDios(fa);
+            vD.setVisible(true);
+        }else{ //Ventana principal Usuario Vivo
+            VUsuarioVivo vUV;
+            vUV=new VUsuarioVivo(fa);
+            vUV.setVisible(true);
+        }
+    }
+    
+    public void muestraVJuzgar(java.awt.Frame parent, boolean modal,Vivo vivo){
+        VJuzgar vj;
+        vj = new VJuzgar(parent,modal,vivo);
+        vj.setVisible(true);
+    }
+    
+    public void muestraVEstadisticas(java.awt.Frame parent, boolean modal){
+        
+    }
+     
+    public void muestraVRegistrar(java.awt.Frame parent, boolean modal){
+       
+    }
+    
+}
